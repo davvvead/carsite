afa34ad0788e89c4141a5d18db34b388d982e1c2
Davvead <davead461@gmail.com>
2022-04-28 21:27:32 +0000

    footer and logo section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  content: ['./dist'],
+  content: ['./dist/index.html'],
   theme: {
     container: {
       center: true,
       padding: '2rem'
     },
-    extend: {},
+    extend: {
+       colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'carglass': '#F2F2F2',
+      'metallic': '#BFBFBF',
+      'vinyllight': '#8C8C8C',
+      'vinyldark': '#404040',
+      'tyre': '#0D0D0D',
+    },
+    },
   },
   plugins: [],
 }
